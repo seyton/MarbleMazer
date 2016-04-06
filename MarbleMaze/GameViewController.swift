1d790f6ea3db5fe82d71dfcfdd7eb6b39ffa3730
Wesley Matlock <wes@insoc.net>
2016-04-06 08:40:11 -0500

    setup more collision detection

--- MarbleMaze/GameViewController.swift
+++ MarbleMaze/GameViewController.swift
@@ -11,9 +11,16 @@ import SceneKit
 
 class GameViewController: UIViewController {
 
+    let CollisionCategoryBall   = 1
+    let CollisionCategoryStone  = 2
+    let CollisionCategoryPillar = 4
+    let CollisionCategoryCrate  = 8
+    let CollisionCategoryPearl  = 16
+    
     var scnView: SCNView!
     var scnScene: SCNScene!
-
+    var ballNode: SCNNode!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,11 +38,12 @@ class GameViewController: UIViewController {
         
         scnScene = SCNScene(named: "art.scnassets/game.scn")
         scnView.scene = scnScene
-        
     }
     
     func setupNodes() {
         
+        ballNode = scnScene.rootNode.childNodeWithName("ball", recursively: true)
+        ballNode.physicsBody?.contactTestBitMask = CollisionCategoryPillar | CollisionCategoryCrate | CollisionCategoryPearl
     }
     
     func setupSounds() {
